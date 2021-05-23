5414c69abff2b47fd2879be3a3edf33182da7bfb
Paul-l-sining <paul.l.sining@gmail.com>
2021-05-23 11:31:15 +0800

    create solve func

--- main.py
+++ main.py
@@ -11,6 +11,25 @@ board = [
 ]
 
 
+def solve(bo):
+    find = find_empty(bo)
+    # Check if there is empty space
+    if not find:
+        return True
+
+    row, col = find
+    for i in range(1, 10):
+        if isValid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+
 def isValid(bo, num, pos):
     # Check row
     for i in range(len(bo[0])):
@@ -28,7 +47,7 @@ def isValid(bo, num, pos):
 
     for i in range(box_y * 3, box_y * 3 + 3):
         for j in range(box_x * 3, box_x * 3 + 3):
-            if bo[i][j] == num and [i, j] != pos:
+            if bo[i][j] == num and (i, j) != pos:
                 return False
 
     return True
@@ -55,6 +74,10 @@ def find_empty(bo):
             if bo[i][j] == 0:
                 return i, j  # row, col
 
+    return None  # there is no empty space
+
 
 print_board(board)
-print(find_empty(board))
+solve(board)
+print("------------------------")
+print_board(board)
